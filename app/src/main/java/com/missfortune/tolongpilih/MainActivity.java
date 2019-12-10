dc224a7072818a57f9e25b0a96a178d527d65bc6
MNFiT3 <mnfitv@gmail.com>
2019-12-11 01:07:26 +0800

    Added example code for server connection

--- app/src/main/java/com/missfortune/tolongpilih/MainActivity.java
+++ app/src/main/java/com/missfortune/tolongpilih/MainActivity.java
@@ -3,12 +3,57 @@ package com.missfortune.tolongpilih;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.missfortune.tolongpilih.services.ServerHandler;
+import com.missfortune.tolongpilih.services.Session;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+    Session session;
+    ServerHandler serverHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        serverHandler = new ServerHandler();
+        session = new Session(getApplicationContext());
+
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("name", "21313");
+            postData.put("address", "23123");
+            postData.put("manufacturer", "233123");
+            postData.put("location", "3213");
+            postData.put("type", "421");
+            postData.put("deviceID", "1231");
+
+            ServerHandler serverHandler = (ServerHandler) new ServerHandler().execute("http://10.0.2.2:3001/api/tolongpilih/test", postData.toString());
+            String temp = serverHandler.get();
+
+            JSONObject result = new JSONObject(temp);
+            String strPostData = result.getString("PostData");
+
+            JSONObject data = new JSONObject(strPostData);
+            String name = data.getString("name");
+
+            TextView txt01 = findViewById(R.id.txt01);
+            txt01.setText(name);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ app/src/main/java/com/missfortune/tolongpilih/services/Session.java
@@ -0,0 +1,30 @@
+package com.missfortune.tolongpilih.services;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Session {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    Context ctx;
+
+    public Session(Context ctx){
+        this.ctx = ctx;
+        sharedPreferences = ctx.getSharedPreferences("app", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    public void isLoggedIn(boolean isLogged, String userEmail){
+        editor.putBoolean("loggedInMode", isLogged);
+        editor.putString("userEmail", userEmail);
+        editor.commit();
+    }
+
+    public boolean loggedIn() {
+        return sharedPreferences.getBoolean("loggedInMode", false);
+    }
+
+    public String userEmail() {
+        return sharedPreferences.getString("userEmail", "");
+    }
+}
