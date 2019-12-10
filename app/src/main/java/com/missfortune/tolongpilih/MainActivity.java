bd47a1125fc489ee6d9e4b1b57bf8ea9b4e4f586
MNFiT3 <mnfitv@gmail.com>
2019-12-11 03:05:13 +0800

    Comment code

--- app/src/main/java/com/missfortune/tolongpilih/MainActivity.java
+++ app/src/main/java/com/missfortune/tolongpilih/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //startActivity(new Intent(this, LoginActivity.class));
-        startActivity(new Intent(this, WheelActivity.class));
+        //startActivity(new Intent(this, WheelActivity.class));
     }
 }
