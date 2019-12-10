29bc0f18605b1f6adec5ff72ed3e27ae7482143a
MNFiT3 <mnfitv@gmail.com>
2019-12-11 02:24:19 +0800

    Login and Logout API

--- app/src/main/java/com/missfortune/tolongpilih/MainActivity.java
+++ app/src/main/java/com/missfortune/tolongpilih/MainActivity.java
@@ -4,56 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import com.missfortune.tolongpilih.config.Globals;
-import com.missfortune.tolongpilih.services.ServerHandler;
-import com.missfortune.tolongpilih.services.Session;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-    Session session;
-    ServerHandler serverHandler;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         startActivity(new Intent(this, LoginActivity.class));
-        serverHandler = new ServerHandler();
-        session = new Session(getApplicationContext());
-
-
-        JSONObject postData = new JSONObject();
-        try {
-            postData.put("username", "21313");
-            postData.put("email", "23123");
-            postData.put("password", "233123");
-
-            ServerHandler serverHandler = (ServerHandler) new ServerHandler().execute(Globals.API_ENDPOINT + "/tolongpilih/test", postData.toString());
-            String temp = serverHandler.get();
-
-            JSONObject result = new JSONObject(temp);
-            String strPostData = result.getString("PostData");
-
-            JSONObject data = new JSONObject(strPostData);
-            String name = data.getString("username");
-
-            TextView txt01 = findViewById(R.id.txt01);
-            txt01.setText(name);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
 
     }
 }
--- app/src/main/java/com/missfortune/tolongpilih/config/Globals.java
+++ app/src/main/java/com/missfortune/tolongpilih/config/Globals.java
@@ -2,4 +2,10 @@ package com.missfortune.tolongpilih.config;
 
 public class Globals {
     public static String API_ENDPOINT = "http://10.0.2.2:3001/api";
+
+    //Route: tolongpilih
+    public static String API_TEST = "/tolongpilih/test";
+
+    //Route: user
+    public static String API_REGISTER_USER = "/user/register";
 }
