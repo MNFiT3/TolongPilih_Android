152215294703e8e085a5d56cc1e7a19454acf638
MNFiT3 <mnfitv@gmail.com>
2019-12-11 23:14:11 +0800

    Workable wheel

--- app/src/main/assets/assets/services/TolongPilihService.js
+++ app/src/main/assets/assets/services/TolongPilihService.js
@@ -2,7 +2,18 @@
 //if (token == null || token == undefined || token == '') {
 //    window.location.href = 'login.html'
 //}
+var token, groupId, API_ENDPOINT, ENDPOINT, data
 
-const onLoad = (groupId, token) => {
-    document.getElementById('test').innerHTML = groupId + " " + token
+const onLoad = (groupId, token, api_endpoint, endpoint, callback) => {
+ localStorage.setItem('token', token)
+ API_ENDPOINT = api_endpoint
+ ENDPOINT = endpoint
+ this.groupId = groupId
+ this.endpoint = endpoint
 }
+
+
+onLoad("66a6e2c4-bd4f-4589-b0bb-0049c34104dd",
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJhODIwMjI3ZC1lOGI3LTQ1NjYtYmZhNi0wOTRmYTc0ZDRkZjEiLCJ1c2VybmFtZSI6IkFkbWluIiwiaWF0IjoxNTc2MDc0NTY2LCJleHAiOjE1NzYwNzgxNjZ9.fgqTbpLEcAJ-EFSoajwg5HvNruCfz0KWzMQ_yeLHmA8',
+'http://localhost:3001/api',
+'/tolongpilih/group/item/list')
