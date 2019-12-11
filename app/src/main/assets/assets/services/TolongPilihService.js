0bceba9c260aef070ced539f8196d06523ce4595
MNFiT3 <mnfitv@gmail.com>
2019-12-12 01:14:09 +0800

    Delete unused file

--- app/src/main/assets/assets/services/TolongPilihService.js
+++
@@ -1,6 +0,0 @@
-//let token = localStorage.getItem('token')
-//if (token == null || token == undefined || token == '') {
-//    window.location.href = 'login.html'
-//}
-
-const onLoad = (groupId, token) => {}
